d4b08fa73ccce1c5135932444ab93e9c80f00dc0
eduardopvieira <eduardoopazvieira@gmail.com>
2025-09-10 18:24:29 -0300

    initial commit

---
+++ src/main/java/br/edu/ufersa/mimic/MimicApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.mimic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufersa/mimic/MimicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.mimic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
