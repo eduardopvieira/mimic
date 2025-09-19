498e04439bdbf160cb6d9fe2ca802f2676e17f0d
eduardopvieira <eduardoopazvieira@gmail.com>
2025-09-19 15:39:33 -0300

    repository de subraça

---
+++ src/main/java/br/edu/ufersa/mimic/repository/caracteristicas/SubracaRepository.java
@@ -0,0 +1,6 @@
+package br.edu.ufersa.mimic.repository.caracteristicas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubracaRepository extends JpaRepository<SubracaRepository, Long> {
+}
