ed19a01eb215b65aa4d44fa04e2f32b977882abb
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-10-06 20:47:01 -0300

    configurando segurança

--- src/main/java/br/edu/ufersa/mimic/repository/auth/UsuarioRepository.java
+++ src/main/java/br/edu/ufersa/mimic/repository/auth/UsuarioRepository.java
@@ -3,8 +3,7 @@ package br.edu.ufersa.mimic.repository.auth;
 import br.edu.ufersa.mimic.model.auth.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String email);
+    Usuario findByEmail(String email);
 }
